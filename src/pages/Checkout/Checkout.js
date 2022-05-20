2f2fac8a2a5a9527c5e59c1ac6aa6f9a7d155d8a
unknown <shahrukhamin27@gmail.com>
2022-05-21 00:15:40 +0600

    checkout-page modify

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -8,80 +8,65 @@ import useServiceDetails from '../../CustomHooks/useServiceDetails';
 
 
 const Checkout = () => {
-    const {id}=useParams()
+    const { id } = useParams()
 
     //useServiceDetails hook call to get each service data based on serviceId
-    const [service]=useServiceDetails(id)  //id=serviceId both same here 
-    const nameRef=useRef('')
-    const phoneRef=useRef('')
-    const emailRef=useRef('')
-    const adressRef=useRef('')
-    const [close,setClose]=useState(false)
+    const [service] = useServiceDetails(id)  //id=serviceId both same here 
+    const nameRef = useRef('')
+    const phoneRef = useRef('')
+    const emailRef = useRef('')
+    const adressRef = useRef('')
+    const [close, setClose] = useState(false)
     //existing logged in user ar info in user
     const [user] = useAuthState(auth);
     // console.log(user.email)
-    
 
-    const submitHandler=(e)=>{
+
+    const submitHandler = (e) => {
         e.preventDefault()
-        const userEmail=emailRef.current.value
-        const userName=nameRef.current.value
-        const userPhone=phoneRef.current.value
-        const userAdress=adressRef.current.value
-        if(user){
+        const userEmail = emailRef.current.value
+        const userName = nameRef.current.value
+        const userPhone = phoneRef.current.value
+        const userAdress = adressRef.current.value
+        if (user) {
             alert(`Thank you ${userName} for the booking!`)
         }
     }
-    const navigate=useNavigate()
+    const navigate = useNavigate()
 
-    const btnCliked=()=>{
+    const btnCliked = () => {
         setClose(!close)
-        if(close){
+        if (close) {
             navigate('/home')
         }
     }
     return (
         <>
-        <div className='checkoutStyle'>
-        <p>CheckOut Page for: {service.name}</p>
-            <div style={{display:'flex',justifyContent:'space-between'}}>
-                <h4>Thank You for Selecting this '{service.name}' package</h4>
-                <button onClick={btnCliked} style={{borderRadius:'5px',color:'teal'}}>X</button>
+            <div className='checkoutStyle'>
+                <p>CheckOut Page for: {service.name}</p>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <h4>Thank You for Selecting this '{service.name}' package</h4>
+                    <button onClick={btnCliked} style={{ borderRadius: '5px', color: 'teal' }}>X</button>
+                </div>
+
+                <div className='container w-50 mx-auto'>
+                    <h3 style={{ color: 'lightBlue', textAlign: 'center', marginTop: '50px' }}>CHECK-OUT</h3>
+                    <Form onSubmit={submitHandler}>
+
+                        <input className='w-100 mb-2' type="text" value='' name="name" placeholder='name' required readOnly disabled />
+                        <br />
+                        <input className='w-100 mb-2' type="email" value='' name="email" placeholder='email' required readOnly disabled />
+                        <br />
+                        <input className='w-100 mb-2' type="text" value='' name="service" placeholder='service' required readOnly />
+                        <br />
+                        <input className='w-100 mb-2' type="text" name="address" placeholder='address' autoComplete='off' required />
+                        <br />
+                        <input className='w-100 mb-2' type="text" name="phone" placeholder='phone' required />
+                        <br />
+                        <input className='btn btn-primary' type="submit" value="Place Order" />
+                    </Form>
+                </div>
             </div>
-
-            <div className='container w-50 mx-auto'>
-                <h3 style={{ color: 'lightBlue', textAlign: 'center', marginTop: '50px' }}>CHECK-OUT</h3>
-                <Form onSubmit={submitHandler}>
-
-                    <Form.Group className="mb-3" controlId="formBasicName">
-                        <Form.Label>Name</Form.Label>
-                        <Form.Control className='w-75' name='name' ref={nameRef}  type="text" placeholder="Name" required />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicAdress">
-                        <Form.Label>Adress</Form.Label>
-                        <Form.Control className='w-75' name='adress' ref={adressRef}  type="text" placeholder="Adress" required />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control className='w-75' name='email' value={user?.email} ref={emailRef}  type="email" placeholder="Enter email" required />
-                        <Form.Text className="text-muted">
-                        </Form.Text>
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Phone Number</Form.Label>
-                        <Form.Control className='w-75' name='phone number' ref={phoneRef}  type="text" placeholder="phone number" required />
-                    </Form.Group>
-
-                    
-                    <button style={{backgroundColor:'teal',padding:'8px',color:'white',borderRadius:'10px',border:'1px solid teal'}} type="submit">
-                        SUBMIT
-                    </button>
-                </Form>
-            </div>
-        </div>
         </>
     );
 };
